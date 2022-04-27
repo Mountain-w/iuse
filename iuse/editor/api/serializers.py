3b7b186ff5fb33fded279b942ce450af4f49b444
Mountain_w <478960464@qq.com>
2022-04-27 20:02:28 +0800

    modify

--- iuse/editor/api/serializers.py
+++ iuse/editor/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from editor.models import Content
 from sources.api.serializers import SourceSerializer
+from sources.models import Source
+from rest_framework.exceptions import ValidationError
 
 
 class ContentSerializer(serializers.ModelSerializer):
@@ -9,3 +11,25 @@ class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
         fields = ('file', 'content',)
+
+
+class FilterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Source
+        fields = ('id',)
+
+    def check_type(self, name):
+        import re
+        pattern = re.compile(r".+\.(?:txt|md|py|js)")
+        result = re.findall(pattern, name)
+        if not result:
+            return False
+        return True
+
+    def validate(self, data):
+        name = self.context['name']
+        if not self.check_type(name):
+            raise ValidationError({"message": f'[{name}]---The type doesn\'t export now'})
+        return data
+
--- iuse/editor/api/views.py
+++ iuse/editor/api/views.py
@@ -4,7 +4,8 @@ from sources.models import Source
 from utils.modelshelpers.enums import FileType, DeleteStatus
 from utils.permissions import IsSourceOwner
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ContentSerializer
+from .serializers import ContentSerializer, FilterSerializer
+from rest_framework.decorators import action
 
 
 class EditorViewSet(viewsets.GenericViewSet):
@@ -15,7 +16,28 @@ class EditorViewSet(viewsets.GenericViewSet):
 
     def retrieve(self, request, pk):
         instance = self.get_object()
+        serializer = FilterSerializer(data=request.data, context={'name': instance.name})
+        if not serializer.is_valid():
+            return Response({
+                'error': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
         content = instance.content
         return Response({
             'content': ContentSerializer(content).data
+        }, status=status.HTTP_200_OK)
+
+    @action(methods=["POST"], detail=True, permission_classes=(IsAuthenticated, IsSourceOwner))
+    def modify(self, request, pk):
+        instance = self.get_object()
+        text = request.data.get('content')
+        if text is None:
+            return Response({
+                'errors': "content is required"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        # 直接将内容放到数据库中，不修改文件
+        content = instance.content
+        content.content = text
+        content.save()
+        return Response({
+            'status': 'success'
         }, status=status.HTTP_200_OK)
